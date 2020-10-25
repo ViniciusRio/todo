8221d2aa97a748001a50f73368c2adf7ce3568af
Vinicius Rio <vlmdr@outlook.com>
2020-10-25 19:09:00 -0400

    crud feito

--- src/index.php
+++ src/index.php
@@ -1,3 +1,24 @@
+<?php
+    $taskFile = file_get_contents('todo-list.json');
+    $tasksJson = json_decode($taskFile, true);
+    $taskId = intval($_GET['todo-id-edit']);
+
+    $taskTitle = '';
+    $taskCurrent = '';
+
+    if ($taskId) {
+        foreach ($tasksJson as $key => $value) {
+            if ($value['id'] === $taskId) {
+                $taskTitle = $key;
+                $taskCurrent = $value;
+            }
+        }
+
+    }
+
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,24 +30,28 @@
     <form action="storage.php" method="post">
         <fieldset>
             <label for="todo-title">Title</label>
-            <input type="text" name="todo-title" id="todo-title">
-
-            <!-- <label for="todo-description">Description</label>
-            <input type="text" name="todo-description" id="todo-description"> -->
+            <input type="text" name="todo-title" id="todo-title" value="<?= $taskTitle ?>"/>
 
             <label for="todo-completed">Completed?</label>
-                <input type="checkbox" name="todo-completed" id="todo-completed"> 
-
-            <button type="submit">Save</button>
+                <input type="checkbox" name="todo-completed" id="todo-completed" <?= $taskCurrent['completed'] ? 'checked' : '' ?>> 
+            <button type="submit"><?=$taskTitle ? 'Update' : 'Save'?></button>
         </fieldset>
+        <?php if ($taskCurrent['id']): ?>
+            <input type="hidden" name="todo-id" value="<?=$taskCurrent['id']?>">
+        <?php endif; ?>
     </form>
 
-    <?php foreach ($jsonArray as $todoTitle => $todo) : ?>
+    <?php foreach ($tasksJson as $todoTitle => $todo) : ?>
         <dl>
         <form action="delete.php" method="post">
             <input type="hidden" name="todo-id" value=<?=$todo['id']?>/>
-            <dt><?= $todoTitle ?><button type="submit">delete</button></dt>
+            <button type="submit">delete</button>
         </form>
+        <!-- <form action="" method="get"> -->
+            <!-- <input type="hidden" name="todo-id-edit"/> -->
+            <a style="text-decoration:none;" href="?todo-id-edit=<?= $todo['id'] ?>"><?= $todoTitle ?></a>
+
+        <!-- </form> -->
         <form action="change-status.php" method="post">
             <input type="hidden" name="todo-id-completed" value=<?=$todo['id']?>/>
             <input type="checkbox" name="todo-check" <?= $todo[completed] ? 'checked' : '' ?> />
@@ -34,6 +59,12 @@
         </dl>
     <?php endforeach; ?>
     <script>
+    const titleTodo = document.querySelectorAll('dt');
+    titleTodo.forEach(title => {
+        title.onclick = function() {
+            this.parentNode.submit();
+        };
+    });
     const checkbox = document.querySelectorAll('input[type=checkbox][name=todo-check]');
     checkbox.forEach(ch => {
         ch.onclick = function() {
--- src/storage.php
+++ src/storage.php
@@ -1,15 +1,60 @@
 <?php
+function dieAndDump($value) {
+    echo "<pre>";
+            die(var_dump($value));
+    echo "</pre>";
+
+}
+
+function change_key($array,$old_key,$new_key) {
+
+    if(!array_key_exists($old_key,$array)) {
+        return $array;
+    }
+
+    $keys = array_keys($array);
+    $keys[array_search($old_key, $keys)] = $new_key;
+
+    return array_combine($keys, $array);
+}
+
+error_reporting();
+ini_set('display_errors',1);
 
     $todoTitle = $_POST['todo-title'] ?? '';
     $todoTitle = trim($todoTitle);
+    $todoIdUpdate = intval($_POST['todo-id']);
+    $fileName = __DIR__ . '/' . 'todo-list.json';
+    $isCompleted = $_POST['todo-completed'] ? true : false;
 
+    if ($todoIdUpdate) {
+        $tasksFile = file_get_contents($fileName);
+        $tasksJsonArray = json_decode($tasksFile, true);
+        
+        foreach ($tasksJsonArray as $title => $value) {
+            if ($value['id'] === $todoIdUpdate) {
+                $tasksUpdate = change_key($tasksJsonArray, $title, $todoTitle);
+
+                file_put_contents($fileName, json_encode($tasksUpdate, JSON_PRETTY_PRINT));
+
+              }
+            
+         }
+
+
+        header('Location: index.php');
+
+
+        return;
+
+    }
+    
     if ($todoTitle) {
-        $fileName = __DIR__ . '/' . 'todo-list.json';
-        $isCompleted = $_POST['todo-completed'] ? true : false;
 
         if (file_exists($fileName)) {
             $json = file_get_contents($fileName);
             $jsonArray = json_decode($json, true);
+
         } else {
             $jsonArray = [];
         }

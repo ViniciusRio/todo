5f95653bc631edc26cb1928cdbaf911b09439a36
Vinicius Rio <vlmdr@outlook.com>
2021-01-01 23:02:45 -0400

    navbar todo

--- src/app/views/partials/nav.php
+++ src/app/views/partials/nav.php
@@ -1,6 +1,11 @@
 <nav>
+    <input type="checkbox" id="check">
+    <label for="check" class="checkbtn">
+        <i class="fas fa-bars"></i>
+    </label>
+    <label class="logo">Todo App</label>
     <ul>
-        <li><a href="/">Home</a></li>
+        <li><a class="active" href="/">Home</a></li>
         <li><a href="/about">About</a></li>
         <li><a href="/contact">Contact</a></li>
         <li><a href="/users">Manage Users</a></li>

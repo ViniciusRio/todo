c64a4e587a74351863034545bbf3a0e8a9620eed
Vinicius Rio <vlmdr@outlook.com>
2021-01-06 23:53:14 -0400

    footer

--- src/app/views/index.view.php
+++ src/app/views/index.view.php
@@ -1,36 +1,37 @@
 
 <?php require DIR_BASE_NAME . '/app/views/partials/head.php'; ?>
+<div class="content">
+    <a href="create">Create new todo</a>
+    <?php if (isset($flash)): ?>
+        <p><?=$flash;?></p>
+    <?php endif; ?>
 
-<a href="create">Create new todo</a>
-<?php if (isset($flash)): ?>
-    <p><?=$flash;?></p>
-<?php endif; ?>
+    <?php if (is_array($todos) && !empty($todos)): ?>
+        <?php foreach ($todos as $todoTitle => $todo) : ?>
+        <dl>
+            <form action="delete" method="POST">
+                <input type="hidden" name="todo-id" value=<?=$todo['id']; ?> />
+                <button type="submit">delete</button>
+            </form>
 
-<?php if (is_array($todos) && !empty($todos)): ?>
-    <?php foreach ($todos as $todoTitle => $todo) : ?>
-    <dl>
-        <form action="delete" method="POST">
-            <input type="hidden" name="todo-id" value=<?=$todo['id']; ?> />
-            <button type="submit">delete</button>
-        </form>
+            <form action="edit" method="GET">
+                <input type="hidden" name="todo-id" value=<?=$todo['id']; ?> />
+                <button type="submit"><?= $todoTitle ?></button>
+            </form>
 
-        <form action="edit" method="GET">
-            <input type="hidden" name="todo-id" value=<?=$todo['id']; ?> />
-            <button type="submit"><?= $todoTitle ?></button>
-        </form>
+            <form action="status/change" method="post">
+                <input type="hidden" name="todo-id-completed" value=<?=$todo['id']?>>
+                <input type="checkbox" name="todo-check" <?= $todo['completed'] ? 'checked' : '' ?> />
+            </form>
+        </dl>
+    <?php endforeach; ?>
+    <?php elseif (empty($todos)): ?>
+        <p>File empty</p>
 
-        <form action="status/change" method="post">
-            <input type="hidden" name="todo-id-completed" value=<?=$todo['id']?>>
-            <input type="checkbox" name="todo-check" <?= $todo['completed'] ? 'checked' : '' ?> />
-        </form>
-    </dl>
-<?php endforeach; ?>
-<?php elseif (empty($todos)): ?>
-    <p>File empty</p>
-
-<?php else:?>
-    <p><?=$todos;?></p>
-<?php endif; ?>
+    <?php else:?>
+        <p><?=$todos;?></p>
+    <?php endif; ?>
+</div>
 
 <script>
     const titleTodo = document.querySelectorAll('dt');
--- src/app/views/partials/footer.php
+++ src/app/views/partials/footer.php
@@ -1,2 +1,46 @@
+<footer class="footer-distributed">
+        <div class="footer-left">
+            <h3>Todo App</h3>
+            <div class="footer-links">
+                <a href="#">Home</a>
+                ·
+                <a href="#">New Todo</a>
+            </div>
+            <p class="footer-company-name">Todo App &copy; 2021</p>
+        </div>
+
+        <div class="footer-center">
+
+            <div class="content-footer-center">
+                <i class="fa fa-map-marker"></i>
+                <p><span>Porto Velho</span> Rondônia, Brazil</p>
+            </div>
+
+            <div class="content-footer-center">
+                <i class="fa fa-phone"></i>
+                <p>+55 (69) 123456</p>
+            </div>
+
+            <div class="content-footer-center">
+                <i class="fa fa-envelope"></i>
+                <p><a href="mailto:support@todoapp.com">support@todoapp.com</a></p>
+            </div>
+
+        </div>
+
+        <div class="footer-right">
+            <p class="footer-company-about">
+                <span>About Todo App</span> Todo App description &amp; Todo App.
+            </p>
+
+            <div class="footer-icons">
+                <a href="#"><i class="fa fa-facebook"></i></a>
+                <a href="#"><i class="fa fa-twitter"></i></a>
+                <a href="#"><i class="fa fa-linkedin"></i></a>
+                <a href="#"><i class="fa fa-github"></i></a>
+            </div>
+        </div>
+</footer>
+ 
 </body>
 </html>
